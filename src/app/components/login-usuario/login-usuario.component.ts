4081877db622b5f09000c770165522bdac5bc7f5
rafaelMunoz77 <rafael.munoz77@gmail.com>
2020-10-21 17:52:16 +0200

    primer ejemplo de formulario - arcaico

--- src/app/components/login-usuario/login-usuario.component.ts
+++ src/app/components/login-usuario/login-usuario.component.ts
@@ -12,4 +12,24 @@ export class LoginUsuarioComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  autenticaUsuario() {
+    var jsonObject = {
+      usuario: (<HTMLInputElement>document.getElementById("usuario")).value,  // Utilizo el id de los campos del formulario
+      password: (<HTMLInputElement>document.getElementById("password")).value
+    };
+
+    console.log("u: " + jsonObject.usuario + " - p: " + jsonObject.password);
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("respuestaDelServidor").innerHTML = this.response;
+      }
+    };
+    xhttp.open("POST", "http://localhost:8080/usuario/autentica", true);
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(JSON.stringify(jsonObject));
+  }
+
 }
