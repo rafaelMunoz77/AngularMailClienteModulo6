e35681a848e2cfd07e36e2e82da808189f66909e
rafaelMunoz77 <rafael.munoz77@gmail.com>
2020-10-24 14:37:13 +0200

    Módulo 6 - Vídeo 14 - Nuevo servicio para recibir mensajes del usuario

--- src/app/components/listado-mensajes/listado-mensajes.component.ts
+++ src/app/components/listado-mensajes/listado-mensajes.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { MensajeService } from '../../services/mensaje.service';
 
+/**
+ * Decorador que permite que esta clase sea un componente, se debe especificar:
+ * - Un selector para poder utilizarlo en HTML y otros muchos lugares
+ * - Un "template", un fichero HTML que muestre la cara de este componente
+ * - Una hoja de estilos.
+ */
 @Component({
   selector: 'app-listado-mensajes',
   templateUrl: './listado-mensajes.component.html',
   styleUrls: ['./listado-mensajes.component.scss']
 })
+
+/**
+ * Esta clase contiene toda la lógica asociada al componente que permite mostrar y operar con los
+ * mensajes del usuario autenticado.
+ */
 export class ListadoMensajesComponent implements OnInit {
 
-  constructor() { }
+  /**
+   * A través del constructor llamo al inyector de objetos y le pido uno de tipo MensajeService
+   */
+  constructor(private mensajeService: MensajeService) { }
 
+  /**
+   * Hook a la inicialización del componente, lo usaré para pedir un listado de mensajes
+   */
   ngOnInit(): void {
+    this.mensajeService.getListadoMensajes(0, 10).subscribe(data => {
+      console.log(data);
+    });
   }
 
 }
---
+++ src/app/services/mensaje.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+
+// El decorador @Inyectable permite que el inyector de código cree una instancia de esta
+// clase en el constructor de otras clases
+@Injectable({
+  providedIn: 'root'
+})
+
+/**
+ * Esta clase aglutina todos los métodos relacionados con obtener mensajes, enviar mensajes,
+ * cambiar el estado de los mensajes, etc.
+ */
+export class MensajeService {
+
+  /**
+   * En este constructor pido al inyector de código que cree variables
+   * @param http    ->  para poder hacer peticiones asíncronas por protocolo http.
+   */
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Pide una página (conjunto ordenador de mensajes) al servidor. La paginación es necesaria.
+   */
+  getListadoMensajes(pagina: number, lineasPorPagina: number): Observable<object> {
+    return this.http.get<object>('http://localhost:8080/mensajes/recibidos?pagina=' + pagina + 
+      '&mensajesPorPagina=' + lineasPorPagina).pipe(
+//      tap(data => console.log(data)), // Si deseas hacer algo con los datos obtenidos, puedes hacerlo en esta línea
+    );
+  }
+
+}
